a8001e716b1500a39cf7f9b9ce0c35f81886b930
Samuel Camilo <scamilo.dev@gmail.com>
2022-04-23 04:34:59 -0300

    ajustes iniciais do projeto

---
+++ 02-dt-money/src/App.tsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+// Ao definir o component com export function, eu consigo
+// importa-lo com o nome do componente, não conseguindo definir
+// uma nomenclatura diferente. Ex: import { App } from "./App";
