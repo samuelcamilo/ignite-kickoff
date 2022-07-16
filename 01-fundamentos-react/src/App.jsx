893f6b311eb1fc1b8e219469ab2f1d8cad98405d
Samuel Camilo <scamilo.dev@gmail.com>
2022-07-16 03:59:10 -0300

    REFACTOR: ajustando repositório

---
+++ 01-fundamentos-react/src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from "./Post";
+
+function App() {
+  return (
+    <>
+      <Post
+        author="Samuel Camilo"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, iure quis ex reprehenderit veniam provident earum hic ea dicta fuga odit sequi maxime aperiam, a itaque quam temporibus non rem!"
+      />
+      <Post
+        author="Beatriz Felix"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, iure quis ex reprehenderit veniam provident earum hic ea dicta fuga odit sequi maxime aperiam, a itaque quam temporibus non rem!"
+      />
+    </>
+  );
+}
+
+export default App;
